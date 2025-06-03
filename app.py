29ad90b821e3206e3703ceaf9d1b943b5d3f3b2d
Juan Daniel Mora <juandaniellesmes23@gmail.com>
2025-06-03 18:31:24 -0500

    formas de resaltar minimos

--- app.py
+++ app.py
@@ -2,11 +2,19 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("covid_data.csv")
+# df2 = df.sample(n=1000, random_state=42)
+numeric_cols = df.select_dtypes(include='number').columns
+
+# st.dataframe(df2.style.highlight_max(axis=0)) # máx filas 262144
 
 def main():
     st.title("Curso de Streamlit")
     st.header("Dataframe:")
     st.dataframe(df)
+    st.dataframe(df.style.highlight_min(axis=0, subset=numeric_cols))
+    st.text(df.dtypes)
+    # st.dataframe(df.style.highlight_max(axis=0)) # máx filas 262144
+    # st.table(df) #solo para tablas pequeñas
     
 
 if __name__ == '__main__':
