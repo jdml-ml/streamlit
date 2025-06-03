66172c5437004ffa730d1939051afde83eb26492
Juan Daniel <juan.mora@bioinsoluciones.com>
2025-06-03 15:35:45 -0500

    uso de ´st.write´ para texto, markdown y sumas

--- app.py
+++ app.py
@@ -2,18 +2,9 @@ import streamlit as st
 
 def main():
     st.title("Curso de Streamlit")
-    st.header("Esto es un encabezado")
-    # Formateo de texto con variables
-    var = "Juan"
-    st.text(f"Hola {var}, esto es un texto")
-    st.markdown("## Markdown --> h2")
-    
-    st.success("Éxito")
-    st.warning("Esto es una advertencia")
-    st.info("Esto es información")
-    st.error("Esto es un error")
-    
-    st.exception("Esto es una excepción")
+    st.write("Texto")
+    st.write("## Esto es un markdown")
+    st.write(1+2)
 
 if __name__ == '__main__':
     main()
