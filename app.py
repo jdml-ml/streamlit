2b1f0b3877ad2110face207c53df6cb4f06b6d94
Juan Daniel Mora <juandaniellesmes23@gmail.com>
2025-06-03 21:54:38 -0500

    cambios básicos en página y <meta>

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 import streamlit as st
-import pandas as pd
-from PIL import Image
+
+st.set_page_config(page_title="Mi App Streamlit", page_icon="smile", layout="wide", initial_sidebar_state="collapsed")
 
 def main():
     st.title("Curso de Streamlit")
-    #Captura de inputs
-    nombre = st.text_input("Ingresa tu nombre")
-
-    mensaje = st.text_area("Ingresa tu mensaje", height=100)
-
-    numero = st.number_input("Ingresa un número", 1.0, 25.0, step=1.0) # min, max
-
-    cita = st.date_input("Selecciona una fecha")
-
-    hora = st.time_input("Selecciona la hora")
-
-    color = st.color_picker("Selecciona un color")
+    st.sidebar.header("Navegación")
     
 
 if __name__ == '__main__':
