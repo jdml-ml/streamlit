6369efd31dd9a2a699196b428201e8a0b7a74c95
Juan Daniel Mora <juandaniellesmes23@gmail.com>
2025-06-03 21:47:56 -0500

    otros elementos básicos text input, area, numero, fecha, hora, color

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from PIL import Image
 
 def main():
     st.title("Curso de Streamlit")
-    img = Image.open("Julia.png")
-    st.image(img, use_container_width=True)
-    st.write("Tomada de: PantheraLeo1359531, CC0, via Wikimedia Commons")
-    # Imagen aleatoria de internet
-    st.image("https://picsum.photos/1920")
+    #Captura de inputs
+    nombre = st.text_input("Ingresa tu nombre")
 
-    # with open("tu_video.mp4", 'rb') as video_file:
-    #     st.video(video_file.read(), start_time=0)
+    mensaje = st.text_area("Ingresa tu mensaje", height=100)
 
-    # with open("tu_audio.mp3", 'rb') as audio_file:
-    #     st.audio(audio_file.read(), start_time=0)
+    numero = st.number_input("Ingresa un número", 1.0, 25.0, step=1.0) # min, max
+
+    cita = st.date_input("Selecciona una fecha")
+
+    hora = st.time_input("Selecciona la hora")
+
+    color = st.color_picker("Selecciona un color")
     
 
 if __name__ == '__main__':
