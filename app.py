f76ddcba711461844923a25015383facc6a2a4e6
Juan <juandaniellesmes23@gmail.com>
2025-06-05 15:55:56 -0500

    (En proceso) carga de archivos

--- app.py
+++ app.py
@@ -4,13 +4,37 @@ import pandas as pd
 import docx2txt
 from PyPDF2 import PdfReader
 
-st.set_page_config(page_title="Mi App Streamlit", page_icon="smile", layout="wide", initial_sidebar_state="collapsed")
+@st.cache_data
+def cargar_imagen(image):
+    img = Image.open(image)
+    return img
 
-df = pd.read_csv("covid_data.csv")
+def leer_pdf(file):
+    pdfReader = PdfReader(file)
+    count = len(pdfReader.pages)
+    text = ""
+    for i in range(count):
+        pagina = pdfReader.pages[i]
+        text += pagina.extract_text()
+    return text
 
 def main():
     st.title("Curso de Streamlit")
-    st.sidebar.header("Navegación")
+    menu = ["Imágenes", "Conjunto de datos", "Archivos de documentos"]
+    eleccion = st.sidebar.selectbox("Menú", menu)
+    if eleccion == "Imágenes":
+        st.header("Imagen")
+        archivo = st.file_uploader("Subir imágenes", type=["png", "jpg", "jpeg"])
+        if archivo is not None:
+            detalle_archivo = {"nombre_archivo": archivo.name,
+                            "tipo_archivo": archivo.type,
+                            "tamanio_archivo": archivo.size}
+            st.write(detalle_archivo)
+        
+    elif eleccion == "Conjunto de datos":
+        st.header("Conjunto de datos")
+    elif eleccion == "Archivo de documentos":
+        st.header("Archivo de documentos")
 
 if __name__ == '__main__':
     main()
