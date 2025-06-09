7a87f7949b5def90afee128606c111ff88ac27c0
Juan <juandaniellesmes23@gmail.com>
2025-06-09 13:36:14 -0500

    carga de archivos

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def leer_pdf(file):
 
 def main():
     st.title("Curso de Streamlit")
-    menu = ["Imágenes", "Conjunto de datos", "Archivos de documentos"]
+    menu = ["Imágenes", "Conjunto de datos", "Archivo de documentos"]
     eleccion = st.sidebar.selectbox("Menú", menu)
     if eleccion == "Imágenes":
         st.header("Imagen")
@@ -30,11 +30,42 @@ def main():
                             "tipo_archivo": archivo.type,
                             "tamanio_archivo": archivo.size}
             st.write(detalle_archivo)
+            st.image(cargar_imagen(archivo), width=350)
         
     elif eleccion == "Conjunto de datos":
         st.header("Conjunto de datos")
+        archivo_datos = st.file_uploader("Subir CSV o Excel", type=["csv", "xls", "xlsx"])
+        if archivo_datos is not None:
+            detalle = {"nombre_archivo": archivo_datos.name,
+                            "tipo_archivo": archivo_datos.type,
+                            "tamanio_archivo": archivo_datos.size}
+            st.write(detalle)
+            if detalle["tipo_archivo"] == "text/csv":
+                df = pd.read_csv(archivo_datos)
+            elif detalle["tipo_archivo"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                df = pd.read_excel(archivo_datos, engine='openpyxl')
+            else:df=pd.DataFrame()
+            st.dataframe(df)
+            
     elif eleccion == "Archivo de documentos":
         st.header("Archivo de documentos")
+        archivo_doc = st.file_uploader("Subir documento", type=["pdf", "docx", "txt"])
+        if st.button("Procesar"):
+            if archivo_doc is not None:
+                detalle = {"nombre_archivo": archivo_doc.name,
+                            "tipo_archivo": archivo_doc.type,
+                            "tamanio_archivo": archivo_doc.size}
+                st.write(detalle)
+                if detalle["tipo_archivo"] == "text/plain":
+                    text = str(archivo_doc.read(), "utf-8")
+                    st.write(text)
+                elif archivo_doc.type == "application/pdf":
+                    text = leer_pdf(archivo_doc)
+                    st.write(text)
+                elif archivo_doc.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                    text = docx2txt.process(archivo_doc)
+                    st.write(text)
+                
 
 if __name__ == '__main__':
     main()
